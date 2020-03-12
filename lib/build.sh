5111bf3ed0a6d764bcbf8d2a64bd51b5f0552b03
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-03-12 08:56:01 -0700

    logic to also include headers in libclang packaging

--- lib/build.sh
+++ lib/build.sh
@@ -5,6 +5,8 @@ mkdir -p clang-build && \
         -DCMAKE_BUILD_TYPE=Release \
         ../llvm && \
     make -j${CPU_COUNT} && \
+    mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include && \
     cp bin/clang-format $PREFIX/bin/clang-format && \
     cp bin/clang-tidy $PREFIX/bin/clang-tidy && \
-    cp lib/libclang.so $PREFIX/lib/libclang.so
+    cp lib/libclang.so $PREFIX/lib/libclang.so && \
+    cp -r ../llvm/tools/clang/lib/Headers $PREFIX/include/ClangHeaders
